ab292939c3b7ce3129b0580ca60428d7197a392c
Egor Mitrofanov <82968933+MaybeeYou@users.noreply.github.com>
2025-06-08 18:26:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,61 @@
-// Copyright 2022 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "tree.h"
+#include "tree.h"
+#include <vector>
 
+std::vector<std::vector<char>> getAllPerms(const PMTree& tree) {
+  return tree.getAllPerms();
+}
+
+std::vector<char> getPerm1(const PMTree& tree, int num) {
+  PMNode* root = tree.getRoot();
+
+  if (!root || root->children.empty() || num <= 0)
+    return {};
+
+  std::vector<std::vector<char>> all = tree.getAllPerms();
+
+  if (num > static_cast<int>(all.size()))
+    return {};
+
+  return all[num - 1];
+}
+
+bool findNthPath(PMNode* node, std::vector<char>& path, int target, int& current) {
+  path.push_back(node->value);
+
+  if (node->children.empty()) {
+    if (current == target)
+      return true;
+    else {
+      current++;
+      path.pop_back();
+      return false;
+    }
+  }
+
+  for (int i = 0; i < node->children.size(); ++i) {
+    if (findNthPath(node->children[i], path, target, current))
+      return true;
+  }
+
+  path.pop_back();
+  return false;
+}
+
+
+std::vector<char> getPerm2(const PMTree& tree, int num) {
+  std::vector<char> result;
+  int counter = 1;
+
+  PMNode* root = tree.getRoot();
+
+  if (!root || root->children.empty())
+    return {};
+
+  for (int i = 0; root && i < root->children.size(); ++i) {
+    if (findNthPath(root->children[i], result, num, counter)) {
+      return result;
+    }
+  }
+
+  return {};
+}
